d0235c0c89aa16eebbf632a6486cc834f3844ff1
Mayur S Kulkarni <48925065+mayursk2000@users.noreply.github.com>
2021-10-18 23:48:22 +0530

    Update footer.php

--- blog/admin/footer.php
+++ blog/admin/footer.php
@@ -1,1 +1,1 @@
-<footer><p>All right reserved. Template by: <a href="http://webthemez.com">WebThemez</a></p></footer>
+<footer><p>All right reserved. Template by: <a href="http://webthemez.com"></a></p></footer>
